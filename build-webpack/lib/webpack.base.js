ac7bb0c4f1a1b3c2705d3536314af93c8a9c6024
betty_yin <291694016@qq.com>
2019-12-15 15:08:34 +0800

    配置包目录设计和eslint规范

---
+++ build-webpack/lib/webpack.base.js
@@ -0,0 +1,123 @@
+const {
+  CleanWebpackPlugin,
+} = require('clean-webpack-plugin');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const setMPA = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
+  Object.keys(entryFiles).map((index) => {
+    const entryFile = entryFiles[index];
+    /**
+     * [ 'D:/webpackDemo/src/index/index.js','D:/webpackDemo/src/search/index.js' ]
+     */
+    const matches = entryFile.match(/src\/([^/]*)\/index\.js$/);
+    const pageName = matches && matches[1];
+    entry[pageName] = entryFile;
+    return htmlWebpackPlugins.push(new HtmlWebpackPlugin({
+      template: path.join(__dirname, `src/${pageName}/index.html`),
+      filename: `${pageName}.html`,
+      // chunks不填写会默认将所有的chunk都引入到html页面，
+      // chunks的填写也是有顺序的，会根据此处数组的顺序引入到html文件中
+      chunks: pageName === 'search' ? ['vendors', 'commons', pageName] : ['commons', pageName],
+      // chunks: [pageName],
+      minify: {
+        html5: true,
+        minifyCSS: true,
+        minifyJS: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: false,
+        removeComments: false,
+      },
+    }));
+  });
+  return {
+    entry,
+    htmlWebpackPlugins,
+  };
+};
+
+const {
+  entry,
+  htmlWebpackPlugins,
+} = setMPA();
+
+module.exports = {
+  entry,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              // 转换的小数位数
+              remPrecision: 8,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                autoprefixer(),
+              ],
+            },
+          },
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        // use: 'file-loader'
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 20 * 1024,
+          },
+        }],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css',
+    }),
+    new CleanWebpackPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    function errorPlugin() {
+      this.hooks.done.tap('done', (stats) => {
+        if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') === -1) {
+          console.log('build error'); // eslint-disable-line
+          process.exit(1);
+        }
+      });
+    },
+  ].concat(htmlWebpackPlugins),
+  stats: 'errors-only',
+};
---
+++ build-webpack/lib/webpack.prod.js
@@ -0,0 +1,49 @@
+const merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const cssnano = require('cssnano');
+const baseConfig = require('./webpack.base');
+
+
+const prodConfig = {
+  mode: 'production',
+  plugins: [
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [{
+        module: 'react',
+        entry: 'https://cdn.bootcss.com/react/16.10.2/umd/react.production.min.js',
+        global: 'React',
+      },
+      {
+        module: 'react-dom',
+        entry: 'https://cdn.bootcss.com/react-dom/16.10.2/umd/react-dom.production.min.js',
+        global: 'ReactDOM',
+      },
+      ],
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        // vendor: {
+        //   test: /(react|react-dom)/,
+        //   // name对应的是entry里面的每个chunk，这样就可以根据不同的html文件引入不同的chunk
+        //   name: 'vendors',
+        //   chunks: 'all'
+        // },
+        commons: {
+          minSize: 0,
+          name: 'commons',
+          minChunks: 2,
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+module.exports = merge(baseConfig, prodConfig);
